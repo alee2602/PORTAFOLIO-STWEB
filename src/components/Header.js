7d96b6e73fe290ebe339db9082cbe9b0b3a7dcc8
Mónica Salvatierra <sal22249@uvg.edu.gt>
2024-05-09 23:26:45 -0600

    Added new js and scss files. Initial header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import styles from '@/styles/components/header.module.scss';
+
+function Header() {
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+            <h3>Salvatierra</h3>
+            </div>
+            <nav className={styles.nav}>
+            <Link href="#home" className={styles.link}>Home</Link>
+            <Link href="#about" className={styles.link}>About Me</Link>
+            <Link href="#projects" className={styles.link}>Projects</Link>
+            <Link href="#contact" className={styles.link}>Contact</Link>
+            </nav>
+        </div>
+        );
+    }
+
+export default Header;
