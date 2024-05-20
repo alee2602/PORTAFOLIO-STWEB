9e8ceb8f4c7626b94e7653e4d30b8775bb2a88ea
Mónica Salvatierra <sal22249@uvg.edu.gt>
2024-05-19 20:49:04 -0600

    finished skills section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,33 +7,33 @@ function AboutSection() {
     const [skillType, setSkillType] = useState('');
 
     const handleShowSkills = (type) => {
-        setSkillType(type);
-        setShowSkills(true);
+    setSkillType(type);
+    setShowSkills(true);
     };
 
     return (
-        <div className={styles['about-section']}>
-            <div className={styles['content-container']}>
-                <div className={styles['header-text']}>
-                    <h1>Hello! I'm Mónica</h1>
-                </div>
-                <div className={styles['content-wrapper']}>
-                    <div className={styles.picture}>
-                        <img src="/pic.jpg" alt="Monica's Profile Picture" />
-                    </div>
-                    <div className={styles['about-text']}>
-                        <p>I’m a passionate third-year Computer Science student with a knack for both front-end and back-end web development. My journey in tech has led me to a fascinating crossroad where design meets functionality, enabling me to craft seamless digital experiences.</p>
-                        <br />
-                        <p>Beyond the world of web development, I'm also captivated by the boundless possibilities within data science.</p>
-                    </div>
-                </div>
-                <div className={styles.skills}>
-                    <button className={styles['skill-button']} onClick={() => handleShowSkills('hard')}>Hard Skills</button>
-                    <button className={styles['skill-button']} onClick={() => handleShowSkills('soft')}>Soft Skills</button>
-                </div>
-                {showSkills && <SkillsSection skillType={skillType} />}
+    <div className={styles['about-section']}>
+        <div className={styles['content-container']}>
+        <div className={styles['header-text']}>
+            <h1>Hello! I'm Mónica</h1>
+        </div>
+        <div className={styles['content-wrapper']}>
+            <div className={styles.picture}>
+            <img src="/pic.jpg" alt="Monica's Profile Picture" />
+            </div>
+            <div className={styles['about-text']}>
+            <p>I’m a passionate third-year Computer Science student with a knack for both front-end and back-end web development. My journey in tech has led me to a fascinating crossroad where design meets functionality, enabling me to craft seamless digital experiences.</p>
+            <br />
+            <p>Beyond the world of web development, I'm also captivated by the boundless possibilities within data science.</p>
             </div>
         </div>
+        <div className={styles.skills}>
+            <button className={styles['skill-button']} onClick={() => handleShowSkills('hard')}>Hard Skills</button>
+            <button className={styles['skill-button']} onClick={() => handleShowSkills('soft')}>Soft Skills</button>
+        </div>
+        {showSkills && <SkillsSection skillType={skillType} />}
+        </div>
+    </div>
     );
 }
 
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,49 +1,86 @@
 import styles from '@/styles/components/skills.module.scss';
 
-function SkillsSection ({skillType}){
+const SkillsSection = ({ skillType }) => {
+const hardSkills = [
+    {
+    category: 'Programming Languages',
+    skills: [
+        { name: 'Python', link: 'https://www.python.org/doc/', svg: '/svgs/python.svg' },
+        { name: 'Java', link: 'https://docs.oracle.com/en/java/', svg: '/svgs/java.svg' },
+        { name: 'Kotlin', link: 'https://kotlinlang.org/docs/home.html', svg: '/svgs/kotlin.svg' },
+        { name: 'C++', link: 'https://cplusplus.com/doc/', svg: '/svgs/cplusplus.svg' },
+        { name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript', svg: '/svgs/javascript.svg' },
+        { name: 'PHP', link: 'https://www.php.net/docs.php', svg: '/svgs/php.svg' },
+            ],
+        },
+    {
+    category: 'Web Development',
+    skills: [
+        { name: 'Next.js', link: 'https://nextjs.org/docs', svg: '/svgs/nextjs.svg' },
+        { name: 'Node.js', link: 'https://nodejs.org/en/docs/', svg: '/svgs/nodejs.svg' },
+        { name: 'React.js', link: 'https://reactjs.org/docs/getting-started.html', svg: '/svgs/react.svg' },
+        { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML', svg: '/svgs/html5.svg' },
+        { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS', svg: '/svgs/css.svg' },
+        { name: 'SASS', link: 'https://sass-lang.com/documentation', svg: '/svgs/sass.svg' },
+        { name: 'Express.js', link: 'https://expressjs.com/en/starter/installing.html', svg: '/svgs/express.svg' },
+            ],
+        },
+    {
+    category: 'Database',
+    skills: [
+        { name: 'MySQL', link: 'https://dev.mysql.com/doc/', svg: '/svgs/mysql.svg' },
+        { name: 'PostgreSQL', link: 'https://www.postgresql.org/docs/', svg: '/svgs/postgresql.svg' },
+            ],
+        },
+    {
+    category: 'Data Visualization',
+    skills: [
+        { name: 'Power BI', link: 'https://docs.microsoft.com/en-us/power-bi/', svg: '/svgs/powerbi.svg' },
+        { name: 'Tableau', link: 'https://www.tableau.com/learn/training', svg: '/svgs/tableau.svg' },
+            ],
+        },
+    ];
 
-    return (
-        <div className={styles['skills-section']}>
-        <h2>My {skillType === 'hard' ? 'Hard' : 'Soft'} Skills</h2>
-        <div className={styles['skills-container']}>
-            {skillType === 'hard' ? (
-            <>
-                <div className={styles.skill}>
-                <h3>Programming Languages</h3>
-                <p>Python, Java, Kotlin, C++, JavaScript, PHP</p>
-                </div>
-                <div className={styles.skill}>
-                <h3>Web Development</h3>
-                <p>Next.js, Node.js, React.js, HTML, CSS, SASS, Express.js</p>
-                </div>
-                <div className={styles.skill}>
-                <h3>Database</h3>
-                <p>MySQL, PostgreSQL</p>
-                </div>
-                <div className={styles.skill}>
-                <h3>Data Visualization</h3>
-                <p>Power BI, Tableu</p>
-                </div>
-            </>
-            ) : (
-            <>
-                <div className={styles.skill}>
-                <h3>Communication</h3>
-                <p>Effective speaking, Active listening</p>
-                </div>
-                <div className={styles.skill}>
-                <h3>Teamwork</h3>
-                <p>Collaboration, Conflict resolution</p>
-                </div>
-                <div className={styles.skill}>
-                <h3>Problem Solving</h3>
-                <p>Critical thinking, Creativity</p>
-                </div>
-            </>
-            )}
+const softSkills = [
+    { name: 'Communication', description: 'Effective speaking, Active listening' },
+    { name: 'Teamwork', description: 'Collaboration, Conflict resolution' },
+    { name: 'Problem Solving', description: 'Critical thinking, Creativity' },  ];
+
+return (
+    <div className={styles.skillsSection}>
+    <h2>My {skillType === 'hard' ? 'Hard' : 'Soft'} Skills</h2>
+    <div className={styles.skillsContainer}>
+        {skillType === 'hard' ? (
+        hardSkills.map((category) => (
+            <div key={category.category} className={styles.skillCategory}>
+            <h3>{category.category}</h3>
+            <div className={styles.skillsList}>
+                {category.skills.map((skill) => (
+                <a
+                    href={skill.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={skill.name}
+                    className={styles.skillIconContainer}
+                    data-tooltip={skill.name}
+                >
+                    <img src={skill.svg} alt={`${skill.name} logo`} className={styles.skillIcon} />
+                </a>
+                ))}
+            </div>
+            </div>
+        ))
+        ) : (
+        softSkills.map((skill) => (
+            <div className={styles.skill} key={skill.name}>
+            <h3>{skill.name}</h3>
+            <p>{skill.description}</p>
+            </div>
+        ))
+        )}
         </div>
-        </div>
-    );
-}
+    </div>
+);
+};
 
 export default SkillsSection;
